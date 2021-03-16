a8407b733aa85c9520d73e0d8ba3b54f7cd679ba
HeshamTB <hishaminv@gmail.com>
2021-03-17 00:21:03 +0300

    Bytearray for mem functions

--- mem.py
+++ mem.py
@@ -14,26 +14,27 @@ class Memory:
         """
         return len(self.mem)
     
-    def write(self, address: int, value: bytes):
+    def write(self, address: int, value: bytearray):
         """
-        Write the passed byte to address
+        Write the passed bytes to address according to size.
         args:
             address: location to write to.
-            value: value as bytes object
+            value: value as bytearray object
         """
-        self.mem[address] = value
-    
+        for i, val in enumerate(value):
+            self.mem[address+i] = value
+
     def read(self, address: int) -> bytes:
         """
         Read address as byte
         """
-        return self.mem[address]
+        return bytearray() + self.mem[address]
 
-    def readHalfWord(self, address: int) -> bytes:
-        return self.mem[address] + self.mem[address+1]
+    # def readHalfWord(self, address: int) -> bytearray:
+    #     return bytearray(self.mem[address] + self.mem[address+1])
     
-    def readWord(self, address: int) -> bytes:
-        return self.mem[address] + self.mem[address+1] + self.mem[address+2] + self.mem[address+3]
+    # def readWord(self, address: int) -> bytearray:
+    #     return bytearray(self.mem[address] + self.mem[address+1] + self.mem[address+2] + self.mem[address+3])
 
     def readAsInt(self, address: int) -> int:
         # TODO check if valid address
@@ -44,12 +45,12 @@ class Memory:
         import os
         values = list()
         for i, val in enumerate(self.mem):
-            values.append(hex(int.from_bytes(val, 'little')))
+            values += hex(int.from_bytes(val, 'little'))+os.linesep
 
-        dum = str()
-        for i, val in enumerate(values):
-           dum = dum + hex(i) +' '+ val + os.linesep
-        return dum
+        # dum = str()
+        # for i, val in enumerate(values):
+        #    dum = dum + hex(i) +' '+ val + os.linesep
+        return values
 
 if __name__ == '__main__':
     x = Memory(50)
